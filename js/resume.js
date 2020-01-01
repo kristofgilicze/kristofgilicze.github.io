dd8de676f94caf33687f8e44531b6677866ff69f
Kristof Gilicze <giliczekristof@gmail.com>
2020-01-01 11:21:49 +0100

    .

--- js/resume.js
+++ js/resume.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', event => {
   window.location.href= "#about";
+
+  window.addEventListener('hashchange', event => {
+    const anchor = `#${ event.newURL.split('#')[1] }`;
+
+    document.querySelectorAll('.anchor-link').forEach(node => {
+      node.classList.remove('active');
+    });
+
+    document.querySelectorAll(`[href="${anchor}"]`)[0].classList.add('active');
+
+  });
 });
